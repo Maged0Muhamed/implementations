be79b47a5cf8dc1d86e7ff768ebc825e1670d0ff
Maged0Muhamed <magdahmed624@gmail.com>
2022-12-19 23:55:44 +0200

    factorial.js Added

---
+++ Algorithm/Factorial.js
@@ -0,0 +1,9 @@
+// Time Complexity O(N) 1 LOOP
+function factorial(n) {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result = result * i;
+  }
+  return result;
+}
+console.log(factorial(5));
